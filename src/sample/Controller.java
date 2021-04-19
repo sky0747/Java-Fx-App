78833d3c3fce740523b9273c7a334e300724c996
sky0747 <vova9862003@gmail.com>
2021-04-20 01:09:42 +0300

    authorisation check

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,4 @@
 package sample;
-import java.io.IOException;
-import java.net.URL;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,6 +7,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import sample.animatons.Shake;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class Controller {
 
@@ -45,20 +47,7 @@ public class Controller {
 
 
         LoginSignUpButon.setOnAction(event -> {
-            LoginSignUpButon.getScene().getWindow().hide();
-
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/sample/signUp.fxml"));
-            try {
-                loader.load();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            Parent root = loader.getRoot();
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.showAndWait();
+            openNewScene("/sample/signUp.fxml");
         });
 
     }
@@ -80,8 +69,94 @@ public class Controller {
             }
             counter++;
         }
-        if (counter >= 1){
-            System.out.println("Success");
+        if (counter >= 1) {
+            openNewScene("/sample/app.fxml");
+        }
+        else {
+            Shake userLoginAnim = new Shake(login_Field);
+            Shake userPassAnim = new Shake(password_Field);
+            userLoginAnim.playAnim();
+            userPassAnim.playAnim();
         }
     }
+    public void openNewScene(String window) {
+        LoginSignUpButon.getScene().getWindow().hide();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(window));
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+    }
 }
+
+
+//    void initialize() {
+//        authSignInButton.setOnAction(event -> {
+//            String loginText = login_Field.getText().trim();
+//            String loginPassword = password_Field.getText().trim();
+//            if(!loginText.equals("") && !loginPassword.equals(""))
+//                loginUser (loginText, loginPassword);
+//            else System.out.println("Please Enter Credentials");
+//        });
+//
+//
+//        LoginSignUpButon.setOnAction(event -> {
+//            LoginSignUpButon.getScene().getWindow().hide();
+//
+//            FXMLLoader loader = new FXMLLoader();
+//            loader.setLocation(getClass().getResource("/sample/signUp.fxml"));
+//            try {
+//                loader.load();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            Parent root = loader.getRoot();
+//            Stage stage = new Stage();
+//            stage.setScene(new Scene(root));
+//            stage.showAndWait();
+//        });
+//
+//    }
+//
+//    private void loginUser(String loginText, String loginPassword) {
+//        DataBaseHandler dbHandler = new DataBaseHandler();
+//        User user = new User();
+//        user.setUserName(loginText);
+//        user.setPassword(loginPassword);
+//        ResultSet result = dbHandler.getUser(user);
+//
+//        int counter = 0;
+//
+//        while (true){
+//            try {
+//                if (!result.next()) break;
+//            } catch (SQLException throwables) {
+//                throwables.printStackTrace();
+//            }
+//            counter++;
+//        }
+//        if (counter >= 1) {
+//
+//
+//        }
+//        else {
+//            Shake userLoginAnim = new Shake(login_Field);
+//            Shake userPassAnim = new Shake(password_Field);
+//            userLoginAnim.playAnim();
+//            userPassAnim.playAnim();
+//        }
+//    }
+//    public void openNewScene(String window) {
+//
+//    }
+//}
+
--- src/sample/animatons/Shake.java
+++ src/sample/animatons/Shake.java
@@ -1,5 +1,21 @@
 package sample.animatons;
 
+import javafx.animation.TranslateTransition;
+import javafx.scene.Node;
+import javafx.util.Duration;
 
 public class Shake {
+    private TranslateTransition tt;
+    public Shake (Node node) {
+        tt = new TranslateTransition(Duration.millis(70), node);
+        tt.setFromX(0f);
+        tt.setByX(10f);
+        tt.setFromY(0f);
+        tt.setByY(5f);
+        tt.setCycleCount(3);
+        tt.setAutoReverse(true);
+    }
+    public void playAnim () {
+        tt.playFromStart();
+    }
 }
